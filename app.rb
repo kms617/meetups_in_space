7de2db3abcb1c420d302f671aebc177e1ccbc013
Kristin <krysia.codes@gmail.com>
2014-09-15 11:36:53 -0400

    core criteria met

--- app.rb
+++ app.rb
@@ -83,4 +83,18 @@ post '/meetups' do
   end
 end
 
+post '/meetups/:meetup_id/memberships' do
+  authenticate!
+
+  meetup = Meetup.find(params[:meetup_id])
+  @membership = Membership.new(user_id: current_user.id, meetup_id: meetup.id)
+
+  if @membership.save
+    flash[:notice] = "You have successfully joined the meetup!"
+    redirect "/meetups/#{meetup.id}"
+  else
+    flash[:notice] = "There was an error; please try again."
+    redirect "/meetups/#{meetup.id}"
+  end
+end
 
